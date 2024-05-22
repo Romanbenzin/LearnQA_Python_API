1e884ef8b7b0c08284c02e78f7f6b3cfcb568e4e
Romanbenzin <mmavrr@mail.ru>
2024-05-22 22:33:15 +0300

    Сделал 4 задание в 4 уроке

--- Lesson_four/again/tests/test_user_register.py
+++ Lesson_four/again/tests/test_user_register.py
@@ -1,23 +1,11 @@
 import requests
 from lib.base_case import BaseCase
 from lib.assertions import Assertions
-from datetime import datetime
 
 class TestUserRegister(BaseCase):
-    def setup_method(self):
-        base_part = "learnqa"
-        domain = "example.com"
-        random_part = datetime.now().strftime("%m%d%Y%H%M%S")
-        self.email = f"{base_part}{random_part}@{domain}"
 
     def test_create_user_successfully(self):
-        data = {
-            "password": "123",
-            "username": "learnqa",
-            "firstName": "learnqa",
-            "lastName": "learnqa",
-            "email": self.email
-        }
+        data = self.prepare_registration_data()
 
         response = requests.post("https://playground.learnqa.ru/api/user/", data=data)
 
@@ -25,13 +13,7 @@ class TestUserRegister(BaseCase):
         Assertions.assert_json_has_key(response, "id")
     def test_create_user_with_existing_email(self):
         email = "vinkotov@example.com"
-        data = {
-            "password": "123",
-            "username": "learnqa",
-            "firstName": "learnqa",
-            "lastName": "learnqa",
-            "email": email
-        }
+        data = self.prepare_registration_data(email)
 
         response = requests.post("https://playground.learnqa.ru/api/user/", data=data)
 
